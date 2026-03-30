8cea03769a8d6fe12d4ace3d9801c23f5dcd3b1f
mmoyaerts <100561325+mmoyaerts@users.noreply.github.com>
2026-03-30 14:21:38 +0200

    init prj

---
+++ avis/src/test/java/fr/esgi/avis/AvisApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.esgi.avis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
